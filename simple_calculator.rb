b0bf48479d0cf18a318f6b315b19b2a891467fb5
anepaz <anelisespaz@gmail.com>
2022-01-05 12:52:15 -0300

    First commit

---
+++ simple_calculator.rb
@@ -0,0 +1,28 @@
+puts "** Simple Calculator **"
+35.times { print "-"}
+puts "\nEnter the first number"
+num_1 = gets.chomp
+puts "Enter the second number"
+num_2 = gets.chomp
+
+puts " Type 1 for multiplication\n Type 2 for division\n Type 3 for addition\n Type 4 for subtraction\n Type 5 for modulus"
+
+input = gets.chomp
+
+case input
+when "1"
+	puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+when "2"
+	puts "The first number divided by the second number is #{num_1.to_i / num_2.to_i}"
+when "3"
+	puts "The first number added by the second number is #{num_1.to_i + num_2.to_i}"
+when "4"
+	puts "The first number subtracted by the second number is #{num_1.to_i - num_2.to_i}"
+when "5"
+	puts "The modulus between the first number and the second is #{num_1.to_i % num_2.to_i}"
+else
+	puts "You typed #{input} - type either 1, 2, 3 or 4 to get the correct answer"
+end
+
+35.times { print "-"}
+puts "\nThanks for using Simple Calculator.\nRefresh to reload!"
